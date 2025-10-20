048b93846e7261e983b767aa7b10c4fcfcab3973
Oleksandr Kuzhel <oleksandrk@remitly.com>
2025-10-20 12:42:13 +0200

    Add SHA256 for v1.1.0 release

--- Formula/renew-wifi-dhcp.rb
+++ Formula/renew-wifi-dhcp.rb
@@ -3,7 +3,7 @@ class RenewWifiDhcp < Formula
   homepage "https://github.com/oleksandrk-dev/renew-wifi-dhcp"
   license "MIT"
   url "https://github.com/oleksandrk-dev/renew-wifi-dhcp/archive/refs/tags/v1.1.0.tar.gz"
-  sha256 "PLACEHOLDER_WILL_UPDATE_AFTER_TAG"
+  sha256 "bb24f6ede20c90e60f8b85482880284edf2bb2adb5ce571a128398152009ec7d"
   head "https://github.com/oleksandrk-dev/renew-wifi-dhcp.git", branch: "main"
 
   depends_on "sleepwatcher"
